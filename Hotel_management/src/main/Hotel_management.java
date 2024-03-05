97c89ca0ff91fb5c14fa62a5577f4d1b48622e2b
Huynh Tien Phat <2409huynhphat@gmail.com>
2024-03-05 14:54:54 +0700

    Add form login and dashboard

---
+++ Hotel_management/src/main/Hotel_management.java
@@ -0,0 +1,15 @@
+package main;
+
+import dashboard.Dashboard;
+
+public class Hotel_management {
+
+    public static void main(String[] args) {
+//        Dashboard DashboardFrame = new Dashboard();
+//        DashboardFrame.setVisible(true);
+
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+    }
+    
+}
